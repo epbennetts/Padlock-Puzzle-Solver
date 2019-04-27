894db195d00f16b540f128c817175dc7cfe2d359
black-turleneck <49991958+black-turleneck@users.noreply.github.com>
2019-04-28 02:12:34 +1000

    Create TreeStruct.java

---
+++ TreeStruct.java
@@ -0,0 +1,20 @@
+package my_package;
+
+
+
+public class TreeStruct {
+
+	private Node root;
+	//private int tree_depth;?	
+	
+	//constructor
+	public TreeStruct (Node root) {
+		this.root = root;		
+	}
+	
+	public Node root()
+	{
+		return root;
+	}
+	
+}
