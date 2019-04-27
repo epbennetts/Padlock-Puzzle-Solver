77ae6e29ff72fb246844f582a6c25067c2aae508
black-turleneck <49991958+black-turleneck@users.noreply.github.com>
2019-04-28 04:23:54 +1000

    Progress on Node.java
    DID: Made generateChildren method and further developed class complexity
    TODO: equals, siblings fix
    
    Note: May be a bit of a mess cos in the middle of it, but it's 4am

--- Node.java
+++ Node.java
@@ -4,30 +4,38 @@ import java.util.*;
 public class Node {
 	//declare:
 	//consider which of these should be static/protected/etc.
+	//!!!might still have leftover mess from changing imput from String data to int[] digits
 
-	private String data;// /number/string/whatever
+	private String data;// /number/string/whatever	//will delete
+	private int[] digits;
 	private Node parent;
 	private ArrayList<Node> children;
-	private ArrayList<Node> siblings;
+	//private ArrayList<Node> siblings;
 	private int depth;
+	
 	//other
-	private int last_changed; 
+	
 	//index (0,1,2) of last digit changed 
 	//for 1st node (root) avoid this bit of code or set to -1 and catch exc
+	private int last_changed; 
+	private boolean expanded;
 	
 	
 	//could try-catch nullpointer for root but seems clunky
-	public Node(String data, Node parent, ArrayList<Node> children, 
-	ArrayList<Node> siblings) {
-
-		this.data = data;
+	//!!!make input arg be the int[], not string, and translate into int[] in main program 
+	public Node(Node parent, int[] digits, int last_changed) {
+		
+		this.digits = digits;
+		this.last_changed = last_changed;
+		expanded = false;
 		//see if this works
 		if (parent == null) {
 			depth = 0;
+			this.last_changed = -1;
 		}
+		else {
 		this.parent = parent;
-		this.children = children;
-		this.siblings = siblings;
+		}
 		
 		//the idea is to not "arbitrarily" set depth when we create node, so not input arg
 		//but still having it as var so we don't have to traverse whole tree every time
@@ -55,38 +63,70 @@ public class Node {
 	public ArrayList<Node> getSiblings() {
 		//assumes the null parent if statement works
 		if (depth != 0) {
-			return siblings;
+			return siblings;//fix: return children of parent except current child
 		}
 		else {
 			return null;
 		}
 				
 	}
-	
+
 	//static? -> not but maybe when we set it yes
-	//!public int getDepth() {
-	//sth like: depth = parent.depth() + 1;
-	//}
-	
+	public int getDepth() {
+		if (depth != 0) {
+			depth = parent.getDepth() + 1;
+		}
+		return depth;
+	}
+
 	//could make boolean or sth?
 	//!consider making appendNewChildren but prob not nec
-	public void appendChild(Node newChild) {
-		children.add(newChild);
-		//needs to be ordered ArrayList!
+	//problem: not very transparent from outside cos uses private vars
+	//needs to be ordered (for loops should ensure that)
+	//generate all poss children now, filter which we expand later (not .equal prev exp nodes)
+	public ArrayList<Node> generateChildren() {
+		//need: last_changed	
+		
+		//assumes parent null thing works		
+		//i.e. if root (depth=0), then last_changed == -1
+
+		ArrayList<Node> children = new ArrayList<Node>();
+		int[] dig_temp = new int[3];
+		for (int x = 0; x < 3; x++) {
+			if (x != last_changed) {
+				dig_temp = digits;
+				if (dig_temp[x] != 0) {
+					dig_temp[x]--;
+					Node child = new Node(this, dig_temp, x);
+					children.add(child);
+				}
+				dig_temp = digits;
+				if (dig_temp[x] != 9) {
+					dig_temp[x]++;
+					Node child = new Node(this, dig_temp, x);
+					children.add(child);
+				} 			
+			}
+		}
+		return children;		
 	}
+
 	
 	//will probs be relevant for cycles
 	//ALTHOUGH won't need this if check for cycles BEFORE adding children
-	public void deleteNode() {
+	//public void deleteNode() {
 		//sth sth
-	}
+	//}
 	
-	public boolean equals(Node node) {
+	public boolean equals(Node otherNode) {
 //!		if (data is same -check weird Str equals thing, mb do toInteger -  
 //		AND  children are the same -ideally don't have to check order bc they are ordered already, but mb be safe)
 //		{
 //			return true;
 //		}
+		
+		//easy: if digits are same and last_changed is same, then same node (check)
+		//make sure there are no weird things with comparing arrays or ints (but ints easy)
 		return false;
 	}
 	
