fbe34756329415425e0e2635f53c1662a67e32f5
black-turleneck <49991958+black-turleneck@users.noreply.github.com>
2019-04-28 19:07:11 +1000

    Update ThreeDigits.java
    //Done: converted start, goal and forbidden to digits in main program
    //ToDo: create tree; reference nodes from here; expand nodes from here; do BFS;

--- ThreeDigits.java
+++ ThreeDigits.java
@@ -2,6 +2,8 @@ package my_package;
 import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Queue;
+
 
 public class ThreeDigits {
 
@@ -12,16 +14,23 @@ public class ThreeDigits {
 	private static File doc;
 	private static Scanner scan; 
 
-	private static String start, goal;
-	private static String[] forbidden;
+	private static int[] start, goal;
+	//2D array vv
+	private static int[][] forbidden;
 	private ArrayList<Node> expanded;
-	private DataStructure fringe;  //could have diff ones dep on algo
+	//could have diff data structs dep on algo
+	//let's do queue for now (BFS)
+	//Can do PQ for otherst
+	private Queue<Node> fringe;  
 	private int num_expanded;
 	
 	private TreeStruct tree;
 
 
 	public static void main(String[] args){
+		String start_str;
+		String goal_str; 
+		
 		//num args
 		if (args.length != 2){
 			System.out.printf("No solution found.\n");
@@ -41,34 +50,44 @@ public class ThreeDigits {
 			scan = new Scanner(doc);		
 
 			//!check if the going to next line and stuff works properly
-			start = scan.nextLine();
-			goal = scan.nextLine();
+			start_str = scan.nextLine();
+			start = strToDigits(start_str);
+			goal_str = scan.nextLine();
+			goal = strToDigits(goal_str);
 
 			if (scan.hasNextLine()) {
-
 				String forb = scan.nextLine();
-				String[] forbidden = forb.split(",");
+				String[] forb_arr = forb.split(",");
 				//for loop to convert each string array element into number(digit)
+				//assumes 3 digits
+				int[] dig = new int[3];
+				forbidden = new int[forb_arr.length][3];
+				for (int i = 0; i < forb_arr.length; i++) {
+					dig = strToDigits(forb_arr[i]); 
+					forbidden[i] = dig;					
+				}
+				
 			}
 		} 
 		catch (FileNotFoundException exc) {
 			exc.printStackTrace();
 		}
-//...
+		
+		
 
 		
 	}
 
-//could make method .stringToDigit() to convert start, goal and all forbidden
-//	public int[] getDigits() {
-//	int[] digits_num = new int[3];
-//	String[] digits_str = data.split(""); 
-//	//assumes 3 digits
-//	for (int i = 0; i < 3; i++) {
-//		digits[i] = Integer.parseInt(digits_str[i]);
-//	}
-//	return digits;
-//}
+	//recall purpose of static?
+	public static int[] strToDigits(String str) {
+		int[] num_arr = new int[3];
+		String[] str_arr = str.split(""); 
+		//assumes 3 digits
+		for (int i = 0; i < 3; i++) {
+			num_arr[i] = Integer.parseInt(str_arr[i]);
+		}
+		return num_arr;
+	}
 
 
 	//methods
