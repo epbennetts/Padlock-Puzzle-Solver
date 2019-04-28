5539dcc2487411f0bbc5c1cafd2ec4a044438357
black-turleneck <49991958+black-turleneck@users.noreply.github.com>
2019-04-29 03:45:53 +1000

    Update Node.java
    //Did: set and get methods for children; moved generateChildren() to main prog
    //Todo: getPath()

--- Node.java
+++ Node.java
@@ -42,7 +42,6 @@ public class Node {
 
 	//methods
 	
-	//!decide if want to return Str of sth else 
 	public int[] getDigits() {
 		return digits;		
 	}	
@@ -51,24 +50,18 @@ public class Node {
 		return parent;		
 	}
 	
-	public ArrayList<Node> getChildren() {
-		return children;		
-	}
+	//!!
+//	public ArrayList<Node> getChildren() {
+		//could throw nullpointer if we run before running generateChildren()
+		//fixes: try catch (if catch: run generate children). This is so we don't generate them twice every time
+		//honestly we don't even need this method
+	//but actually might be useful still
+//		return children;		
+//	}
 	
-	public int getLastChanged() {
+	public int lastChanged() {
 		return last_changed;
 	}
-	
-//	public ArrayList<Node> getSiblings() {
-//		//assumes the null parent if statement works
-//		if (depth != 0) {
-//			return siblings;//fix: return children of parent except current child
-//		}
-//		else {
-//			return null;
-//		}
-//				
-//	}
 
 	//static? -> not but maybe when we set it yes
 	public int getDepth() {
@@ -78,39 +71,54 @@ public class Node {
 		return depth;
 	}
 
-	//!consider making appendNewChildren but prob not nec
+	//consider an append children fctn also but prob not nec
+	//could make a Boolean, if not succesful returns false
+	public void setChildren(ArrayList<Node> childList) {
+		this.children = childList;
+		//do stuff
+	}
+	
+	public ArrayList<Node> getChildren() {
+		return this.children;
+	}
+	
+	
 	//problem: not very transparent from outside cos uses private vars
 	//needs to be ordered (for loops should ensure that)
 	//generate all poss children now, filter which we expand later (not .equal prev exp nodes)
-	public ArrayList<Node> generateChildren() {
-		//assumes parent null thing works		
-		//i.e. if root (depth=0), then last_changed == -1
-		ArrayList<Node> children = new ArrayList<Node>();
-		int[] dig_temp = new int[3];
-		for (int x = 0; x < 3; x++) {
-			if (x != last_changed) {
-				dig_temp = digits;
-				if (dig_temp[x] != 0) {
-					dig_temp[x]--;
-					Node child = new Node(this, dig_temp, x);
-					children.add(child);
-				}
-				dig_temp = digits;
-				if (dig_temp[x] != 9) {
-					dig_temp[x]++;
-					Node child = new Node(this, dig_temp, x);
-					children.add(child);
-				} 			
-			}
-		}
-		return children;		
-	}
+//	public ArrayList<Node> generateChildren() {
+//		//assumes parent null thing works		
+//		//i.e. if root (depth=0), then last_changed == -1
+//		
+//		//consider making the children in the main program and having a .add() function -why again?
+//		//issue: We're making an arraylist thing but we're not actually "assigning" children to Node
+//		ArrayList<Node> children = new ArrayList<Node>();
+//		int[] dig_temp = new int[3];
+//		for (int x = 0; x < 3; x++) {
+//			if (x != last_changed) {
+//				dig_temp = digits;
+//				if (dig_temp[x] != 0) {
+//					dig_temp[x] = digits[x]-1;
+//					Node child = new Node(this, dig_temp, x);
+//					children.add(child);
+//				}
+//				dig_temp = digits;
+//				if (dig_temp[x] != 9) {
+//					dig_temp[x] = digits[x]+1;
+//					Node child = new Node(this, dig_temp, x);
+//					children.add(child);
+//				} 			
+//			}
+//		}
+//		this.children = children;
+//		return children;		
+//	}
 	
 	public boolean equals(Node otherNode) {
 //		assumptions: if data is same, digits should be same 
 //		if  children are the same, last_changed should be same 
 		int[] other_digits = otherNode.getDigits();
-		int other_changed = otherNode.getLastChanged();
+		int other_changed = otherNode.lastChanged();
 
 		//can use Arrays.equals(), compare indiv contents for more transparency/safer
 		if (Arrays.equals(this.digits, other_digits) && this.last_changed == other_changed) {
@@ -119,14 +127,19 @@ public class Node {
 		return false;
 	}
 
-	//getPath()
+	//getPath() --> put in main progr
 	//This way don't create new one every time, but can get when nec.
 	//iterate through parents until root reached (e.g. while parent != null)
 	//
 		
-
 	
-	//will probs be relevant for cycles
+	//need toStr() as well for when we print at the end
+	//could do the children thing in main program and work with strings here?
+	//but actually v unnecessary during search tbh, so no
+	//could do Integer.toString() for each digit in num
+	//OR Regex!!
+	
+	//might be relevant for cycles
 	//but we could ignore instead of deleting
 	//public void deleteNode() {
 		//sth sth
